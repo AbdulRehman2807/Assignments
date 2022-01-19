67e4fd91433ebbb6eee2c1cab5af6541448b000e
AbdulRehman2807 <mark2359978@gmail.com>
2022-01-19 21:35:24 +0500

    intial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+function moveToLigin(e) {
+  e.preventDefault();
+  window.location.replace(
+    "file:///C:/Users/binary/OneDrive/Desktop/Excer/class%201-12-2022/login.html"
+  );
+  console.log("good");
+}
+
+// moveToLigin();
+function moveTOPage(e){
+    e.preventDefault();
+    window.location.assign('file:///C:/Users/binary/OneDrive/Desktop/Excer/class%201-12-2022/dash.html')
+
+}
