4bc4fb82f27c8bf755a0c8d46f9b838117e1da60
AbdulRehman2807 <mark2359978@gmail.com>
2022-01-28 16:06:09 +0500

    File Address are set

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 function moveToLigin(e) {
   e.preventDefault();
   window.location.replace(
-    "file:///C:/Users/binary/OneDrive/Desktop/Excer/class%201-12-2022/login.html"
+    "file:///C:/Users/binary/OneDrive/Desktop/Excer/Singin-Singup%20form/login.html"
   );
-  console.log("good");
 }
 
 // moveToLigin();
 function moveTOPage(e){
     e.preventDefault();
-    window.location.assign('file:///C:/Users/binary/OneDrive/Desktop/Excer/class%201-12-2022/dash.html')
-
+    window.location.assign(
+      "file:///C:/Users/binary/OneDrive/Desktop/Excer/Singin-Singup%20form/dash.html"
+      );
 }
